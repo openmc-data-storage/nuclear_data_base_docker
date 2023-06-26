209baf6c36f53a7c86f3440f18e66d2e90225675
Jonathan Shimwell <mail@jshimwell.com>
2023-06-26 14:03:13 +0100

    added endf 8

--- convert.py
+++ convert.py
@@ -15,12 +15,23 @@ odj.cross_section_h5_files_to_json_files(
     index_filename='TENDL-2019_index.json',
     indent=None
 )
+odj.cross_section_h5_files_to_json_files(
+    filenames = list(pathlib.Path('ENDFB-8.0-NNDC').glob('*.h5')),
+    output_dir = 'ENDFB-8.0-NNDC_json',
+    library='ENDFB-8.0-NNDC',
+    index_filename='ENDFB-8.0-NNDC_index.json',
+    indent=None
+)
 
+with open('TENDL-2019_json/TENDL-2019_index.json') as f:
+    data_tendl_2019 = json.load(f)
 
-f = open('TENDL-2019_json/TENDL-2019_index.json')
-data = json.load(f)
+with open('ENDFB-8.0-NNDC_json/ENDFB-8.0-NNDC_index.json') as f:
+    data_endf_8_nndc = json.load(f)
+
+data = data_tendl_2019 + data_endf_8_nndc
+
 df = pd.json_normalize(data)
-
 for col in df.columns:
     df[col] = df[col].astype(str)
 
@@ -28,3 +39,4 @@ h5File = "all_indexes.h5"
 df.to_hdf(h5File, "/data/d1")
 
 os.system('rm -rf TENDL-2019')
+os.system('rm -rf ENDFB-8.0-NNDC')
--- download.py
+++ download.py
@@ -2,10 +2,14 @@
 import openmc_data_downloader as odd
 
 
-odd.just_in_time_library_generator(
+odd.download_cross_section_data(
     libraries='TENDL-2019',
     elements='all',
     destination='TENDL-2019'
 )
 
-
+odd.download_cross_section_data(
+    libraries='ENDFB-8.0-NNDC',
+    elements='all',
+    destination='ENDFB-8.0-NNDC',
+)
